7bb4983a2d0f8e9b5687a843d6ad6619284e929a
Dag Lem <dag@nimrod.no>
2023-09-13 21:08:25 +0200

    Added functionality for response validation
    Code adapted from https://github.com/networknt/light-rest-4j

--- src/main/java/com/mservicetech/openapi/validation/OpenApiValidator.java
+++ src/main/java/com/mservicetech/openapi/validation/OpenApiValidator.java
@@ -1,15 +1,21 @@
 package com.mservicetech.openapi.validation;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mservicetech.openapi.common.ParameterType;
 import com.mservicetech.openapi.common.RequestEntity;
+import com.mservicetech.openapi.common.ResponseEntity;
 import com.mservicetech.openapi.common.Status;
 import com.networknt.jsonoverlay.Overlay;
+import com.networknt.oas.model.Header;
+import com.networknt.oas.model.MediaType;
 import com.networknt.oas.model.Operation;
 import com.networknt.oas.model.Parameter;
 import com.networknt.oas.model.Path;
 import com.networknt.oas.model.RequestBody;
+import com.networknt.oas.model.Response;
+import com.networknt.oas.model.Schema;
 import com.networknt.oas.model.impl.RequestBodyImpl;
 import com.networknt.oas.model.impl.SchemaImpl;
 import com.networknt.openapi.ApiNormalisedPath;
@@ -44,6 +50,11 @@ public class OpenApiValidator {
     final String VALIDATOR_REQUEST_PARAMETER_PATH_MISSING = "ERR11108";
     final String VALIDATOR_REQUEST_PARAMETER_HEADER_MISSING = "ERR11017";
     final String VALIDATOR_REQUEST_PARAMETER_QUERY_MISSING = "ERR11000";
+
+    final String VALIDATOR_RESPONSE_CONTENT_UNEXPECTED = "ERR11018";
+    final String REQUIRED_RESPONSE_HEADER_MISSING = "ERR11019";
+
+    final String DEFAULT_STATUS_CODE = "default";
 
 
     public String spec;
@@ -118,25 +129,19 @@ public class OpenApiValidator {
      */
     public Status validateRequestPath (String requestURI , String httpMethod, RequestEntity requestEntity ) {
         requireNonNull(openApiHelper, "openApiHelper object cannot be null");
-        final NormalisedPath requestPath = new ApiNormalisedPath(requestURI, openApiHelper.basePath);
-        final Optional<NormalisedPath> maybeApiPath = openApiHelper.findMatchingApiPath(requestPath);
-        if (!maybeApiPath.isPresent()) {
-            Status status = new Status( STATUS_INVALID_REQUEST_PATH, requestPath.normalised());
-            return status;
-        }
-
-        final NormalisedPath openApiPathString = maybeApiPath.get();
-        final Path path = openApiHelper.openApi3.getPath(openApiPathString.original());
-
-        final Operation operation = path.getOperation(httpMethod.toLowerCase());
-        OpenApiOperation openApiOperation = new OpenApiOperation(openApiPathString, path, httpMethod, operation);
-
-        if (operation == null) {
-            Status status = new Status( STATUS_METHOD_NOT_ALLOWED, requestPath.normalised());
-            return status;
+        OpenApiOperation openApiOperation;
+        try {
+            openApiOperation = getOpenApiOperation(requestURI, httpMethod);
+            if (openApiOperation == null) {
+                return new Status(STATUS_INVALID_REQUEST_PATH, requestURI);
+            }
+        } catch (NullPointerException e) {
+            // This exception is thrown from within the OpenApiOperation constructor on operation == null.
+            return new Status(STATUS_METHOD_NOT_ALLOWED, requestURI);
         }
 
         if (requestEntity!=null) {
+            NormalisedPath requestPath = openApiOperation.getPathString();
             Status status = validateRequestParameters(requestEntity, requestPath, openApiOperation);
             if(status != null) return status;
             if ((requestEntity.getContentType()==null || requestEntity.getContentType().startsWith("application/json"))) {
@@ -350,6 +355,109 @@ public class OpenApiValidator {
     }
 
     /**
+     * validate a given response content object
+     * @param responseContent response content needs to be validated
+     * @param openApiOperation OpenApi Operation which is located by uri and httpMethod
+     * @param statusCode eg. 200, 400
+     * @param mediaTypeName eg. "application/json"
+     * @return Status return null if no validation errors
+     */
+    protected Status validateResponseContent(Object responseContent, OpenApiOperation openApiOperation, String statusCode, String mediaTypeName) {
+        //try to convert json string to structured object
+        if(responseContent instanceof String) {
+            try {
+                responseContent = attachJsonBody((String)responseContent);
+            } catch (Exception e) {
+                return new Status(VALIDATOR_RESPONSE_CONTENT_UNEXPECTED, openApiOperation.getMethod(), openApiOperation.getPathString().original());
+            }
+        }
+        JsonNode schema = getContentSchema(openApiOperation, statusCode, mediaTypeName);
+        //if cannot find schema based on status code, try to get from "default"
+        if(schema == null || schema.isMissingNode()) {
+            // if corresponding response exist but also does not contain any schema, pass validation
+            if (openApiOperation.getOperation().getResponses().containsKey(String.valueOf(statusCode))) {
+                return null;
+            }
+            schema = getContentSchema(openApiOperation, DEFAULT_STATUS_CODE, mediaTypeName);
+            // if default also does not contain any schema, pass validation
+            if (schema == null || schema.isMissingNode()) return null;
+        }
+        if ((responseContent != null && schema == null) ||
+                (responseContent == null && schema != null)) {
+            return new Status(VALIDATOR_RESPONSE_CONTENT_UNEXPECTED, openApiOperation.getMethod(), openApiOperation.getPathString().original());
+        }
+        SchemaValidatorsConfig config = new SchemaValidatorsConfig();
+        config.setTypeLoose(false);
+        config.setHandleNullableField(true);
+        return schemaValidator.validate(responseContent, schema, config);
+    }
+
+    /**
+     * Validate the response against the given API operation
+     * @param requestURI normalised path
+     * @param httpMethod Http method definition (get/post/put/delete)
+     * @param statusCode Http status code
+     * @param responseEntity wrapper object for response
+     * @return A validation report containing validation errors
+     */
+    public Status validateResponsePath(String requestURI, String httpMethod, String statusCode, ResponseEntity responseEntity) {
+        OpenApiOperation openApiOperation;
+        try {
+            openApiOperation = getOpenApiOperation(requestURI, httpMethod);
+            if (openApiOperation == null) {
+                return new Status(STATUS_INVALID_REQUEST_PATH, requestURI);
+            }
+        } catch (NullPointerException e) {
+            // This exception is thrown from within the OpenApiOperation constructor on operation == null.
+            return new Status(STATUS_METHOD_NOT_ALLOWED, requestURI);
+        }
+        Status status = validateHeaders(responseEntity.getHeaders(), openApiOperation, statusCode);
+        if(status != null) return status;
+        return validateResponseContent(responseEntity.getContent(), openApiOperation, statusCode, responseEntity.getContentType());
+    }
+
+    private Status validateHeaders(Map<String, ?> headers, OpenApiOperation operation, String statusCode) {
+        Optional<Response> response = Optional.ofNullable(operation.getOperation().getResponse(statusCode));
+        if(response.isPresent()) {
+            Map<String, Header> headerMap = response.get().getHeaders();
+            Optional<Status> optional = headerMap.entrySet()
+                    .stream()
+                    //based on OpenAPI specification, ignore "Content-Type" header
+                    //If a response header is defined with the name "Content-Type", it SHALL be ignored. - https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#responseObject
+                    .filter(entry -> !"Content-Type".equalsIgnoreCase(entry.getKey()))
+                    .map(p -> validateHeader(headers, p.getKey(), p.getValue()))
+                    .filter(s -> s != null)
+                    .findFirst();
+            if(optional.isPresent()) {
+                return optional.get();
+            }
+        }
+        return null;
+    }
+
+    private Status validateHeader(Map<String, ?> headers, String headerName, Header operationHeader) {
+        // According to RFC7230, header field names are case-insensitive.
+        Optional<Object> headerValue = Optional.ofNullable(headers).flatMap(opt -> opt.entrySet()
+                .stream()
+                .filter(entry -> headerName.equalsIgnoreCase(entry.getKey()))
+                .map(entry -> entry.getValue())
+                .findFirst()
+        );
+        if (headerValue.isEmpty()) {
+            if (Boolean.TRUE.equals(operationHeader.getRequired())) {
+                return new Status(REQUIRED_RESPONSE_HEADER_MISSING, headerName);
+            }
+        } else {
+            SchemaValidatorsConfig config = new SchemaValidatorsConfig();
+            //header won't tell if it's a real string or not. needs trying to convert.
+            config.setTypeLoose(true);
+            config.setHandleNullableField(true);
+            return schemaValidator.validate(headerValue.get(), Overlay.toJson((SchemaImpl)operationHeader.getSchema()), config);
+        }
+        return null;
+    }
+
+    /**
      * Method used to parse the body into a Map or a List and attach it into exchange
      *
      * @param bodyString   unparsed request body
@@ -373,6 +481,40 @@ public class OpenApiValidator {
 
         }
         return body;
+    }
+
+    /**
+     * locate operation based on uri and httpMethod
+     * @param requestURI normalised path
+     * @param httpMethod http method of the request
+     * @return OpenApiOperation the wrapper of an api operation
+     */
+    private OpenApiOperation getOpenApiOperation(String requestURI, String httpMethod) {
+        NormalisedPath requestPath = new ApiNormalisedPath(requestURI, openApiHelper.basePath);
+        Optional<NormalisedPath> maybeApiPath = openApiHelper.findMatchingApiPath(requestPath);
+        if (!maybeApiPath.isPresent()) {
+            return null;
+        }
+
+        final NormalisedPath openApiPathString = maybeApiPath.get();
+        final Path path = openApiHelper.openApi3.getPath(openApiPathString.original());
+
+        final Operation operation = path.getOperation(httpMethod);
+        return new OpenApiOperation(openApiPathString, path, httpMethod, operation);
+    }
+
+    private JsonNode getContentSchema(OpenApiOperation operation, String statusCode, String mediaTypeStr) {
+        Schema schema;
+        Optional<Response> response = Optional.ofNullable(operation.getOperation().getResponse(String.valueOf(statusCode)));
+        if(response.isPresent()) {
+            Optional<MediaType> mediaType = Optional.ofNullable(response.get().getContentMediaType(mediaTypeStr));
+            if(mediaType.isPresent()) {
+                schema = mediaType.get().getSchema();
+                JsonNode schemaNode = schema == null ? null : Overlay.toJson((SchemaImpl)schema);
+                return schemaNode;
+            }
+        }
+        return null;
     }
 
     class ValidationResult {
